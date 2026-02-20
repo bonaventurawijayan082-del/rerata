3387205f8a2c85f570f32792dfff7a0df44580ee
BONAVENTURA <BONAVENTURA@LAPTOP-DG0D1S78>
2026-02-20 09:53:03 +0700

    Deklarasi Main

---
+++ src/reratavariabel/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    // a. Deklarasi array
+    public static int[] deklarasiArray() {
+        int[] arr = new int[10];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
+        arr[5] = 6;
+        arr[6] = 7;
+        arr[7] = 8;
+        arr[8] = 9;
+        arr[9] = 10;
+        return arr;
+    }
+        public static void main(String[] args) {
+            int[] arr = deklarasiArray();
+            System.out.println("Array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
